c7f4ce934cbb6c62e8aff6d98f70bc22cda194cd
Kerem BALCI <kerembalci90@gmail.com>
2019-04-07 09:40:03 -0400

    - Add random terrain generation

--- RandomTerrainGenerator/GameScene.swift
+++ RandomTerrainGenerator/GameScene.swift
@@ -10,80 +10,60 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     override func didMove(to view: SKView) {
+        createRandomTerrain()
+    }
+    
+    private func createRandomTerrain() {
+        removeAllChildren()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let map = SKNode()
+        addChild(map)
+        map.xScale = 0.3
+        map.yScale = 0.3
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let tileSet = SKTileSet(named: "Sample Grid Tile Set")!
+        let tileSize = CGSize(width: 128, height: 128)
+        let columns = 128
+        let rows = 128
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        let waterTiles = tileSet.tileGroups.first { $0.name == "Water" }
+        let grassTiles = tileSet.tileGroups.first { $0.name == "Grass" }
+        let sandTiles = tileSet.tileGroups.first { $0.name == "Sand" }
+        
+        let bottomLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
+        bottomLayer.fill(with: sandTiles)
+        map.addChild(bottomLayer)
+        
+        let noiseMap = makeNoiseMap(with: columns, and: rows)
+        let topLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
+        topLayer.enableAutomapping = true
+        map.addChild(topLayer)
+        
+        for column in 0..<columns {
+            for row in 0..<rows {
+                let location = vector2(Int32(row), Int32(column))
+                let terrainHeight = noiseMap.value(at: location)
+                if terrainHeight < 0 {
+                    topLayer.setTileGroup(waterTiles, forColumn: column, row: row)
+                } else {
+                    topLayer.setTileGroup(grassTiles, forColumn: column, row: row)
+                }
+            }
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    private func makeNoiseMap(with columns: Int, and rows: Int) ->GKNoiseMap {
+        let source = GKPerlinNoiseSource(frequency: 2, octaveCount: 3, persistence: 0.5, lacunarity: 2, seed: Int32(arc4random_uniform(UInt32(500 - 1))))
+        let noise = GKNoise(source)
+        let size = vector2(1.0, 1.0)
+        let origin = vector2(0.0, 0.0)
+        let sampleCount = vector2(Int32(columns), Int32(rows))
+        
+        return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        createRandomTerrain()
     }
 }
